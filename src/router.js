66c30ab5715472afd4ffc4a00c605cbfdafac79e
locky <miaojihj@outlook.com>
2019-03-20 15:45:03 +0800

    1

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: './',
+      path: '/',
       name: 'home',
       component: Home,
       // redirect: '/englishCorner',
@@ -18,7 +18,7 @@ export default new Router({
       }
     },
     {
-      path: './program',
+      path: '/program',
       name: 'program',
       component: () => import('./views/Program'),
       meta: {
@@ -27,7 +27,7 @@ export default new Router({
       }
     },
     {
-      path: '../talk',
+      path: '/talk',
       name: 'talk',
       component: () => import('./views/Talk'),
       meta: {
