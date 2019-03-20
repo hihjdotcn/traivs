62ae5c7c37de1b97055e101e16af5406ae145aee
locky <miaojihj@outlook.com>
2019-03-20 15:35:08 +0800

    ii

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/traivs/',
+      path: '/',
       name: 'home',
       component: Home,
       // redirect: '/englishCorner',
